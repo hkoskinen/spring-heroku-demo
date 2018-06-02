c046b51c2044bfeebd75abb1959f485d4a6512f2
Henri Koskinen <hkoskinen@protonmail.com>
2018-06-02 20:50:33 +0300

    Clean code

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -jar target/*.jar
+web: java -jar target/shd-1.0-SNAPSHOT.jar
--- src/main/java/com/gravenium/shd/controllers/HomeController.java
+++ src/main/java/com/gravenium/shd/controllers/HomeController.java
@@ -1,13 +1,13 @@
 package com.gravenium.shd.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     @ResponseBody
     public String home() {
         return "Hello, World!";
